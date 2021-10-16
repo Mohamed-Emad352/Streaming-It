bd988389ad84a767f1559c0c8a8f9d4156b3b2fe
Mohamed Emad <memad352@gmail.com>
2021-10-16 19:08:00 +0200

    Initial commit (Homepage)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'StreamingIt';
-}
+export class AppComponent {}
